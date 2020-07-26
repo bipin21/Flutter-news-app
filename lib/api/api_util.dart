39d94a66f5ce4e2c1df5cfb81341f608c440fae1
Bipin karki <bipinkarki172@gmail.com>
2020-07-26 20:47:40 +0545

    Flutter add request

---
+++ lib/api/api_util.dart
@@ -0,0 +1,7 @@
+class ApiUtil{
+  static const String MAIN_API_URL = 'http://bipi-news.loc/api';
+
+  static const String ALL_CATEGORIES = '/categories';
+
+
+}
---
+++ lib/api/categories_api.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'api_util.dart';
+
+class CategoiresApi{
+
+  fetchAllCategories() async{
+    var _client = new HttpClient();
+    _client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+
+    String allCategories = ApiUtil.MAIN_API_URL + ApiUtil.ALL_CATEGORIES + '?q={http}';
+    Map<String,String> headers = {
+      'Accept' : 'application/json'
+    };
+
+    var response = await http.get(allCategories, headers: headers);
+//    var response = await _client.getUrl((Uri.parse(allCategories)));
+//    if(response.statusCode == 200){
+
+//    }
+
+  print(response.statusCode);
+  print(response.body);
+  }
+}
