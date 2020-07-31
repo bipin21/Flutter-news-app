ca741b6a50f3015a0722097d08498ab9fe294ed9
Bipin karki <bipinkarki172@gmail.com>
2020-07-31 17:49:55 +0545

    Fix http client issue

--- lib/api/api_util.dart
+++ lib/api/api_util.dart
@@ -1,5 +1,6 @@
 class ApiUtil{
-  static const String MAIN_API_URL = 'http://bipi-news.loc/api';
+//  static const String MAIN_API_URL = 'http://bipi-news.loc/api';
+  static const String MAIN_API_URL = 'https://bipi-news.herokuapp.com/api';
 
   static const String ALL_CATEGORIES = '/categories';
 
--- lib/api/categories_api.dart
+++ lib/api/categories_api.dart
@@ -2,12 +2,14 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'api_util.dart';
+import 'dart:convert';
+import 'package:flutter_app/models/category.dart';
 
 class CategoiresApi{
 
   fetchAllCategories() async{
-    var _client = new HttpClient();
-    _client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+//    var _client = new HttpClient();
+//    _client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
 
     String allCategories = ApiUtil.MAIN_API_URL + ApiUtil.ALL_CATEGORIES;
     Map<String,String> headers = {
@@ -16,11 +18,19 @@ class CategoiresApi{
 
     var response = await http.get(allCategories, headers: headers);
 //    var response = await _client.getUrl((Uri.parse(allCategories)));
-//    if(response.statusCode == 200){
 
-//    }
+    List<Category> categories = [];
+    if(response.statusCode == 200){
+      Map<String,dynamic> body = jsonDecode(response.body);
 
-  print(response.statusCode);
-  print(response.body);
+      for(var item in body['data']){
+        Category category = Category.fromJson(item);
+        categories.add(category);
+      }
+    }
+    else{
+
+    }
+
   }
 }
